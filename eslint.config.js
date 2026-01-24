8d0718bc89c89088b6feb0c0034b929d9bc1797e
Leejay Hsu <37034741+leejayhsu@users.noreply.github.com>
2026-01-24 09:16:02 -0800

    eslint issues

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
   {
     rules: {
       // override/add rules settings here, such as:
